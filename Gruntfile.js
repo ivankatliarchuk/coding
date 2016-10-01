bec5a23c861a767a7d6d4fcae07135f107d3c43f
ivan <tanzwud.work@gmail.com>
2016-10-01 13:42:11 +0100

    bolwer and yo support. :IvKa

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,37 @@
 
 module.exports = function (grunt) {
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // metadata .
+        jshint: {
+            gruntfile: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: 'Gruntfile.js'
+            },
+            /* can be set for every module */
+            gruntautomation: {
+                src: ['gruntautomation/*.js']
+            }
+        },
+        /* configure watcher */
+        watch: {
+            gruntfile: {
+                files: ['gruntautomation/*.html', 'gruntautomation/*.js'],
+                tasks: ['jshint']
+            }
+        }
+    });
+
     grunt.registerTask('Hello', 'say hello', function () {
         grunt.log.writeln('Hello from grunt');
     });
 
-    grunt.registerTask('default', 'hello');
+    // These plugins provide tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint', 'hello']);
 };
