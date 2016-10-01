d6b6c0127baec0a0fe2ace93c404da758309dfcd
ivan <tanzwud.work@gmail.com>
2016-10-01 14:39:25 +0100

    bower integrated.  :IvKa

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,39 @@
 
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        // metadata .
-        jshint: {
-            gruntfile: {
-                options: {
-                    jshintrc: '.jshintrc'
-                },
-                src: 'Gruntfile.js'
-            },
-            /* can be set for every module */
-            gruntautomation: {
-                src: ['gruntautomation/*.js']
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // metadata .
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
         },
-        /* configure watcher */
-        watch: {
-            gruntfile: {
-                files: ['gruntautomation/*.html', 'gruntautomation/*.js'],
-                tasks: ['jshint']
-            }
-        }
-    });
+        src: 'Gruntfile.js'
+      },
+      /* can be set for every module */
+      gruntautomation: {
+        src: ['app/**/*.js']
+      }
+    },
+    /* configure watcher */
+    watch: {
+      gruntfile: {
+        files: ['app/**/*.html', 'app/**/*.js'],
+        tasks: ['test']
+      }
+    }
+  });
 
-    grunt.registerTask('Hello', 'say hello', function () {
-        grunt.log.writeln('Hello from grunt');
-    });
+  grunt.registerTask('Hello', 'say hello', function () {
+    grunt.log.writeln('Hello from grunt');
+  });
 
-    // These plugins provide tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('test', ['qunit', 'jasmine']);
 
-    grunt.registerTask('default', ['jshint', 'hello']);
+  // These plugins provide tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'hello']);
 };
